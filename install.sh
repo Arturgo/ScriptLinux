596e406ae66289c3b526eec467e500834d3c2eae
Arthur <arturgo@hotmail.fr>
2017-08-30 10:53:06 +0200

    Deuxieme vague

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-yes | apt-get install update
-yes | apt-get install upgrade
+yes | apt-get update
+yes | apt-get upgrade
 yes | apt-get install gimp
 yes | apt-get install chromium-browser
-
+yes | apt-get install pepperflashplugin-nonfree
+yes | apt-get install geogebra
+yes | apt-get install python3
+yes | apt-get install idle3
+yes | apt-get install gparted
+yes | apt-get install blender
+yes | apt-get install skype
+yes | apt-get install inkscape
+yes | apt-get install filezilla
+yes | apt-get install htop
+yes | apt-get install emacs
